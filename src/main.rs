a9f3b2fcc0af284a7e2046a860dfef2965af3355
Andrew Speed <andrew.cspeed@gmail.com>
2024-11-09 22:11:28 +0000

    Refactor to remove some duplication

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use anyhow::Result;
 use clap::Parser;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Read};
 
-#[derive(Parser)]
-struct Args {
+#[derive(Parser, Debug)]
+struct ArgParser {
     filename: Option<String>,
 
     #[arg(short = 'c', long, help = "print the byte counts")]
@@ -19,6 +20,8 @@ struct Args {
     #[arg(short, long, help = "print the word counts")]
     words: bool,
 }
+
+type CounterFn = fn(&str) -> usize;
 
 fn count_file_bytes(input: &str) -> usize {
     input.len()
@@ -39,11 +42,12 @@ fn count_file_words(input: &str) -> usize {
 }
 
 fn main() -> Result<()> {
-    let mut args = Args::parse();
+    let mut args = ArgParser::parse();
 
     let mut output_parts: Vec<String> = vec![];
 
     // set default args if no args provided
+    // TODO: conditional defaults with arg parser builder module?
     if !args.bytes && !args.lines && !args.words && !args.chars {
         args.lines = true;
         args.words = true;
@@ -54,6 +58,7 @@ fn main() -> Result<()> {
     let mut output_filename = None;
 
     // read from stdin if no filename
+    // TODO: extract function
     if let Some(filename) = args.filename {
         let _ = File::open(filename.clone())?.read_to_end(&mut input);
         output_filename = Some(filename);
@@ -63,30 +68,26 @@ fn main() -> Result<()> {
 
     let contents = String::from_utf8(input)?;
 
-    if args.lines {
-        let result = count_file_lines(&contents);
-        output_parts.push(result.to_string());
-    }
+    // TODO: use builder for arg parsing to be able to reference argument by strings
+    let mut arg_function_mapping: HashMap<&str, (bool, CounterFn)> = HashMap::new();
+    arg_function_mapping.insert("lines", (args.lines, count_file_lines));
+    arg_function_mapping.insert("words", (args.words, count_file_words));
+    arg_function_mapping.insert("bytes", (args.bytes, count_file_bytes));
+    arg_function_mapping.insert("chars", (args.chars, count_file_chars));
 
-    if args.words {
-        let result = count_file_words(&contents);
-        output_parts.push(result.to_string());
-    }
-
-    if args.bytes {
-        let result = count_file_bytes(&contents);
-        output_parts.push(result.to_string());
-    }
-
-    if args.chars {
-        let result = count_file_chars(&contents);
-        output_parts.push(result.to_string());
-    }
+    arg_function_mapping
+        .into_iter()
+        .for_each(|(_arg, (should_execute, function))| {
+            if should_execute {
+                output_parts.push(function(&contents).to_string());
+            }
+        });
 
     if let Some(filename) = output_filename {
         output_parts.push(filename);
     }
 
+    // TODO: output formatter
     let output = output_parts.join(" ");
     println!("{output}");
 
