6ca234cbb5d5c28074dee5a9b9e83e6add59422a
Andrew Speed <andrew.cspeed@gmail.com>
2024-11-09 21:07:36 +0000

    Add support for input via stdin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
 use clap::Parser;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{self, Read};
 
 #[derive(Parser)]
 struct Args {
-    filename: String,
+    filename: Option<String>,
 
     #[arg(short = 'c', long, help = "print the byte counts")]
     bytes: bool,
@@ -20,65 +20,80 @@ struct Args {
     words: bool,
 }
 
-fn count_file_bytes(file: &File) -> Result<u64> {
-    let file_bytes = file.metadata()?.len();
-
-    Ok(file_bytes)
+fn count_file_bytes(input: &str) -> Result<u64> {
+    Ok(input.len() as u64)
 }
 
-fn count_file_chars(mut file: &File) -> Result<u64> {
-    let mut file_contents = vec![];
-    file.read_to_end(&mut file_contents)?;
-    let char_count = String::from_utf8(file_contents)?.chars().count();
+fn count_file_chars(input: &str) -> Result<u64> {
+    let char_count = input.chars().count();
 
     Ok(char_count as u64)
 }
 
-fn count_file_lines(file: &File) -> Result<u64> {
-    let reader = BufReader::new(file);
-    let line_count = reader.lines().count();
+fn count_file_lines(input: &str) -> Result<u64> {
+    let line_count = input.lines().count();
     Ok(line_count.try_into().unwrap())
 }
 
-fn count_file_words(file: &File) -> Result<u64> {
-    let reader = BufReader::new(file);
+fn count_file_words(input: &str) -> Result<u64> {
     let mut word_count = 0;
 
-    for line in reader.lines() {
-        word_count += line?.split_whitespace().count();
+    for line in input.lines() {
+        word_count += line.split_whitespace().count();
     }
 
     Ok(word_count.try_into().unwrap())
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
+    let mut args = Args::parse();
 
     let mut output_parts: Vec<String> = vec![];
 
-    let file = File::open(args.filename.clone())?;
-
-    match args.bytes {
-        true => output_parts.push(count_file_bytes(&file)?.to_string()),
-        false => {}
-    };
-
-    match args.lines {
-        true => output_parts.push(count_file_lines(&file)?.to_string()),
-        false => {}
+    // set default args if no args provided
+    if !args.bytes && !args.lines && !args.words && !args.chars {
+        args.lines = true;
+        args.words = true;
+        args.bytes = true;
     }
 
-    match args.words {
-        true => output_parts.push(count_file_words(&file)?.to_string()),
-        false => {}
+    let mut input: Vec<u8> = vec![];
+    let mut output_filename = None;
+
+    // read from stdin if no filename
+    if let Some(filename) = args.filename {
+        let _ = File::open(filename.clone())?.read_to_end(&mut input);
+        output_filename = Some(filename);
+    } else {
+        let _ = io::stdin().read_to_end(&mut input);
     }
 
-    match args.chars {
-        true => output_parts.push(count_file_chars(&file)?.to_string()),
-        false => {}
+    let contents = String::from_utf8(input)?;
+
+    if args.lines {
+        let result = count_file_lines(&contents)?;
+        output_parts.push(result.to_string());
     }
 
-    output_parts.push(args.filename);
+    if args.words {
+        let result = count_file_words(&contents)?;
+        output_parts.push(result.to_string());
+    }
+
+    if args.bytes {
+        let result = count_file_bytes(&contents)?;
+        output_parts.push(result.to_string());
+    }
+
+    if args.chars {
+        let result = count_file_chars(&contents)?;
+        output_parts.push(result.to_string());
+    }
+
+    if let Some(filename) = output_filename {
+        output_parts.push(filename);
+    }
+
     let output = output_parts.join(" ");
     println!("{output}");
 
